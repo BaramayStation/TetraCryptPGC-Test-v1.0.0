935f5d1f619bf0dd2aa6ecad207e2fe6c47b9452
Abraxas618 <michaeltassmacdonald@outlook.com>
2025-03-11 13:00:27 -0500

    Update __init__.py

--- src/__init__.py
+++ src/__init__.py
@@ -1,1 +1,10 @@
+# src/__init__.py
+from .kyber_kem import kyber_keygen, kyber_encapsulate, kyber_decapsulate
+from .falcon_sign import falcon_keygen, falcon_sign, falcon_verify
+from .handshake import pq_xdh_handshake_mutual
 
+__all__ = [
+    'kyber_keygen', 'kyber_encapsulate', 'kyber_decapsulate',
+    'falcon_keygen', 'falcon_sign', 'falcon_verify',
+    'pq_xdh_handshake_mutual'
+]
