02ea0ca1590f9d4ea86585ede2b83ca7b0ca5f77
Abraxas618 <michaeltassmacdonald@outlook.com>
2025-03-11 20:01:54 -0500

    Update key_rotation.py

--- src/key_rotation.py
+++ src/key_rotation.py
@@ -3,6 +3,13 @@ import hashlib
 from src.kyber_kem import kyber_keygen
 from cryptography.hazmat.primitives.kdf.hkdf import HKDF
 from cryptography.hazmat.primitives import hashes
+import time
+
+ROTATION_INTERVAL = 30 * 24 * 60 * 60  # 30 days
+
+def should_rotate(last_rotation_time):
+    """Check if key rotation is needed based on time elapsed."""
+    return (time.time() - last_rotation_time) > ROTATION_INTERVAL
 
 class KeyRotation:
     def __init__(self, rotation_interval=3600):
